a689e59bee0d1157a13b318f045e94fb1112ca6e
sunssnew <sunssnew@163.com>
2018-05-14 18:18:24 +0800

    第一次提交

---
+++ eureka-server/src/main/java/com/wangfan/springboot/start/EurekaServer.java
@@ -0,0 +1,24 @@
+package com.wangfan.springboot.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer extends SpringBootServletInitializer{
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaServer.class);
+	}
+}
+
+//http://localhost:8761
